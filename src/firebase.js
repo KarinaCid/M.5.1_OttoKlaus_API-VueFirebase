4d1679be4a35db01eb70be21ae465780bb6f7868
Karina Cid <memento_mori.06@hotmail.com>
2020-11-10 22:50:37 -0300

    project structure: router, store, views & firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCmsoORLZ1w5VyJ8I_Z7sSJfvRGd7lm7JU",
+    authDomain: "prueba-firebase-b7b80.firebaseapp.com",
+    databaseURL: "https://prueba-firebase-b7b80.firebaseio.com",
+    projectId: "prueba-firebase-b7b80",
+    storageBucket: "prueba-firebase-b7b80.appspot.com",
+    messagingSenderId: "845654911487",
+    appId: "1:845654911487:web:6cf62cf214137b705c5fca",
+  };
---
+++ src/views/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+      <v-card width="400px" class="mx-auto my-5">
+        <v-card-title class="pb-0">
+        <h2 class="mx-auto mb-5">Juguetes Otto Klaus</h2>
+        </v-card-title>
+        <v-card-text>
+          <v-form>  
+            <v-text-field
+              label="Usuario"
+              prepend-icon="mdi-account-circle"
+              v-model="user"/>
+            <v-text-field
+              label="Contraseña"
+              :type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+              v-model="password"/>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="info" @click.prevent="login">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+  </v-app>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import firebase from "firebase";
+export default {
+  data() {
+    return {
+      user: "",
+      password: "",
+      showPassword: false,
+    };
+  },
+  methods: {
+    ...mapActions(["updateCurrentUser"]),
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.user, this.password)
+        .then(() => {
+          this.updateCurrentUser(firebase.auth().currentUser);
+          this.$router.push("/login ");
+        })
+        .catch(() => {
+          alert("Usuario o Contraseña incorrectos");
+        });
+    },
+  },
+  created() {
+    this.getApod();
+  },
+};
+</script>
